1c1c596242bebf9960de50afb63bb1b259b7018c
Dominik Siebelt <dominik.siebelt@web.de>
2024-02-05 10:32:24 +0100

    extract Param

--- plotspeed.py
+++ plotspeed.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 
+PARAM = "-@"
+
 # ArgumentParser erstellen und Argument hinzufügen
 parser = argparse.ArgumentParser(description='Plot.')
 parser.add_argument('filename', type=str, help='The filename to get the csv data from.')
@@ -17,24 +19,24 @@ print(df.keys())
 
 
 def extractParam(param_string):
-    return param_string.strip().split("-m")[1].strip().split(" ")[0]
+    return param_string.strip().split("-@")[1].strip().split(" ")[0]
 
 
-df['mem'] = df['params'].apply(extractParam)
+df[PARAM] = df['params'].apply(extractParam)
 # print the mem column
-print(df['mem'])
+print(df[PARAM])
 
 # Plot the execution time against the 'mem' column
-ax = df.plot(x='mem', y=" execution_time", kind='line')
+ax = df.plot(x=PARAM, y="execution_time", kind='line')
 
 # Add labels and title
-plt.xlabel('mem')
+plt.xlabel(PARAM)
 plt.ylabel('Execution Time')
-plt.title('Execution Time vs Memory')
+plt.title(f'Execution Time vs {PARAM}')
 
 # Set xticks
 ax.set_xticks(df.index)
-ax.set_xticklabels(df['mem'])
+ax.set_xticklabels(df[PARAM])
 
 # Show the plot
 plt.show()
