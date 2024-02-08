f4445ad26330d7df5eec3d46facaf2f8d6a222e2
Dominik Siebelt <dominik.siebelt@web.de>
2024-02-08 13:50:31 +0100

    Add command line argument for input file and improve file size formatting

--- plotfiles.py
+++ plotfiles.py
@@ -1,20 +1,61 @@
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import json
-
+from typing import List, Union
 import numpy as np
 
-# Function to format large numbers in a human readable format
-def human_format(num):
-    num = float('{:.3g}'.format(num))
-    magnitude = 0
-    while abs(num) >= 1000:
-        magnitude += 1
-        num /= 1000.0
-    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
+# add command line argument for input file
+import argparse
+parser = argparse.ArgumentParser(description='Plot the file sizes and creation times')
+parser.add_argument('input', type=str, help='The input file')
+args = parser.parse_args()
+
+
+METRIC_LABELS: List[str] = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+BINARY_LABELS: List[str] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+PRECISION_OFFSETS: List[float] = [0.5, 0.05, 0.005, 0.0005] # PREDEFINED FOR SPEED.
+PRECISION_FORMATS: List[str] = ["{}{:.0f} {}", "{}{:.1f} {}", "{}{:.2f} {}", "{}{:.3f} {}"] # PREDEFINED FOR SPEED.
+
+
+def human_format(num: Union[int, float], metric: bool=False, precision: int=1) -> str:
+    """
+    Human-readable formatting of bytes, using binary (powers of 1024)
+    or metric (powers of 1000) representation.
+    """
+
+    assert isinstance(num, (int, float)), "num must be an int or float"
+    assert isinstance(metric, bool), "metric must be a bool"
+    assert isinstance(precision, int) and precision >= 0 and precision <= 3, "precision must be an int (range 0-3)"
+
+    unit_labels = METRIC_LABELS if metric else BINARY_LABELS
+    last_label = unit_labels[-1]
+    unit_step = 1000 if metric else 1024
+    unit_step_thresh = unit_step - PRECISION_OFFSETS[precision]
+
+    is_negative = num < 0
+    if is_negative: # Faster than ternary assignment or always running abs().
+        num = abs(num)
+
+    for unit in unit_labels:
+        if num < unit_step_thresh:
+            # VERY IMPORTANT:
+            # Only accepts the CURRENT unit if we're BELOW the threshold where
+            # float rounding behavior would place us into the NEXT unit: F.ex.
+            # when rounding a float to 1 decimal, any number ">= 1023.95" will
+            # be rounded to "1024.0". Obviously we don't want ugly output such
+            # as "1024.0 KiB", since the proper term for that is "1.0 MiB".
+            break
+        if unit != last_label:
+            # We only shrink the number if we HAVEN'T reached the last unit.
+            # NOTE: These looped divisions accumulate floating point rounding
+            # errors, but each new division pushes the rounding errors further
+            # and further down in the decimals, so it doesn't matter at all.
+            num /= unit_step
+
+    return PRECISION_FORMATS[precision].format("-" if is_negative else "", num, unit)
 
 # Load the JSON data
-with open('/home/dominik/ba/results/2024-02-07T18:01:56.518021/fileSizes.json') as f:
+with open(args.input) as f:
     data = json.load(f)
 
 # Prepare the event plot data
@@ -32,7 +73,7 @@ for idx, (file, file_data) in enumerate(data.items()):
         legendLabels.append(process.split('-o')[0].strip())
 
 
-colors = iter(cm.tab20(np.linspace(0, 1, len(lines))))
+colors = iter(cm.tab20b(np.linspace(0, 1, len(lines))))
 
 all_ys = []
 all_xs = []
